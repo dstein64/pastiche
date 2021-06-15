41859e8bdebb07861590cc22bf01847669b0bb85
Dan <dstein64@users.noreply.github.com>
2021-06-15 17:29:26 -0400

    Add numpy as an explicit dependency.
    As of https://github.com/pytorch/pytorch/pull/52794, numpy is a dynamic
    dependency and is not installed automatically when installing torch.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     package_data={'pastiche': ['version.txt', 'vgg19_weights_tf_dim_ordering_tf_kernels_notop_q.bin']},
     packages=['pastiche'],
     python_requires='>=3.6',
-    install_requires=['h5py', 'pillow', 'torch', 'torchvision'],
+    install_requires=['h5py', 'numpy', 'pillow', 'torch', 'torchvision'],
     url='https://github.com/dstein64/pastiche',
     version=version,
 )
